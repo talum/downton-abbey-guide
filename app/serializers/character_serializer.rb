8e9d716c41bc2da0578aef757ec04651820b5f0b
talum <tracidini@gmail.com>
2015-11-29 22:16:02 -0500

    Adds actors to api

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -2,5 +2,6 @@ class CharacterSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :name, :title, :social_class, :actor_id, :family_id
   has_many :quotes
+  has_one :actor
 
 end
