ce4a819ecd7b6f4a25733562e968b19ceaa411c5
talum <tracidini@gmail.com>
2016-01-06 18:43:51 -0500

    Add update to seasons controller

--- app/controllers/api/v1/seasons_controller.rb
+++ app/controllers/api/v1/seasons_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::SeasonsController < ApplicationController
   def index
     render json: Season.all
   end
+
+  def update
+    @season = Season.find(params[:id])
+    @season.update(season_params)
+    render json: @season
+  end
+
+private
+  def season_params
+    params.require(:season).permit(:summary, :time_period, :name, :image_url)
+  end
 end
