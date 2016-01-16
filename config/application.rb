a6c195b158bc1ff4d2820c2496d0a081cd295779
talum <tracidini@gmail.com>
2016-01-16 12:32:32 -0500

    Revert "restore cors origin header"
    This reverts commit 7bb256901596775c465f23d5ba05d5cd14487354.

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module DowntonAbbeyGuide
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'http://downton-abbey-quips.herokuapp.com/'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch, :delete]
       end
     end
